ea1512bd80754b6de36dc61d65777f208136b592
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-06-02 21:54:03 -0300

    consegui fazer com js a animação funcionar, porem ainda não resolvi a animação reversa

--- day-002/js/script.js
+++ day-002/js/script.js
@@ -1,4 +1,13 @@
-$(".menu").bind('click', function() {
-	$(this).toggleClass('active');
-	$(this).find('div').removeClass('no-animation');
-});
+// $(".menu").bind('click', function() {
+// 	$(this).toggleClass('active');
+// 	$(this).find('div').removeClass('no-animation');
+// });
+
+const menu = document.getElementById("menu");
+let line = document.querySelectorAll("#menu > div");
+
+menu.addEventListener("click", () => {
+    line.forEach((el) => {
+        el.classList.toggle("no-animation")
+    })
+})
