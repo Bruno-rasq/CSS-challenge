4c01aa34f5bb30d40cc141e737dd65234da504d0
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-06-02 23:41:17 -0300

    tentei dnv. sem sucesso

--- day-002/js/script.js
+++ day-002/js/script.js
@@ -1,11 +1,18 @@
 const menu = document.getElementById("menu");
 let line = document.querySelectorAll("#menu > div");
 
+// menu.addEventListener("click", () => {
+//     line.forEach((el) => {
+//         el.classList.toggle("no-animation");
+//     })
+// })
+
+
+// tentativa -002
 menu.addEventListener("click", () => {
-    line.forEach((el) => {
-        el.classList.toggle("no-animation");
-    })
+    menu.classList.toggle('active')
+
+    // if(menu.classList.contains('active')){
+    //     menu.classList.add('rev')
+    // }
 })
-
-// menu.classList.toggle("rev")
-
