74a3175409ea9e3832e697675a690c99d96e8879
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-06-08 20:03:11 -0300

    ainda não consegui concluir o desafio do dia 2.

--- day-002/js/script.js
+++ day-002/js/script.js
@@ -8,11 +8,22 @@ let line = document.querySelectorAll("#menu > div");
 // })
 
 
-// tentativa -002
+// // tentativa -002
 menu.addEventListener("click", () => {
-    menu.classList.toggle('active')
-
+    menu.classList.toggle('active');
     // if(menu.classList.contains('active')){
     //     menu.classList.add('rev')
     // }
 })
+
+// tentativa -003
+// menu.addEventListener("click", () => {
+//     menu.classList.toggle('active');
+//     line.forEach((el) => {
+//       el.classList.toggle("no-animation");
+//     })
+
+//     // if(menu.classList.contains('active')){
+//     //     menu.classList.add('rev')
+//     // }
+// })
