9039343a8cf608fdafc736d5cfd6b112127f3142
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-07-12 21:57:38 -0300

    dia 13 concluido

--- day-013/js/script.js
+++ day-013/js/script.js
@@ -1,10 +1,12 @@
 const btns = document.querySelectorAll(".btn");
 const callTop = document.querySelector(".call-top");
 const callBottom = document.querySelector(".call-bottom");
+const icon =document.querySelector(".icon");
 
 btns.forEach((btns) => btns.addEventListener("click", () => {
     callTop.classList.toggle("call-top-active");
     callBottom.classList.toggle("call-bottom-active");
+    icon.classList.toggle("icon-active");
 }));
 
 const close = document.querySelector(".close");
@@ -12,4 +14,5 @@ const close = document.querySelector(".close");
 close.addEventListener("click", () => {
     callTop.classList.toggle("call-top-active");
     callBottom.classList.toggle("call-bottom-active");
+    icon.classList.toggle("icon-active");
 });
